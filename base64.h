90cfb3609475ff92f6b27718a58ed6067411e4a5
David Glaenzel <87229873+2Bor3d@users.noreply.github.com>
2026-06-16 09:43:51 +0200

    feat: added a simple test case

--- base64.h
+++ base64.h
@@ -1,12 +1,14 @@
 #ifndef BASE64_H
 #define BASE64_H
 
-size_t get_encoded_length(size_t length);
+size_t b64_get_encoded_length(size_t length);
 
-int encode_unsigned_int(char *output, unsigned int *input, size_t size);
+int b64_encode(char *output, char *input);
 
-int decode(char *output, char *input);
+int b64_encode_unsigned_int(char *output, unsigned int *input, size_t size);
 
-int decode_unsigned_int(unsigned int *output, char *input);
+int b64_decode(char *output, char *input);
+
+int b64_decode_unsigned_int(unsigned int *output, char *input);
 
 #endif
